ca139a7ca4ab3d382aaf672f2bdd6c940cf30832
SumitRanaware <sumit.ranaware@atyeti.com>
2025-07-08 17:27:03 +0530

    cicd added

--- Personalized-News-Recommender-System/src/main/java/com/example/Personalized/News/Recommender/System/controller/NewsController.java
+++ Personalized-News-Recommender-System/src/main/java/com/example/Personalized/News/Recommender/System/controller/NewsController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://personalizednewsrecommenderapp-react.onrender.com")
 @RestController
 @RequestMapping("/api/news")
 @RequiredArgsConstructor
